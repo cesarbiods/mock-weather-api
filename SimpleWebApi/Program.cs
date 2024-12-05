058178b1621c575491fedbeb191232149023aeca
crincon <rincon.c@northeastern.edu>
2024-12-04 20:15:53 -0500

    Added the influxDB client library.
    Got the system logs to upload to my local instance of influxDB.

--- SimpleWebApi/Program.cs
+++ SimpleWebApi/Program.cs
@@ -1,3 +1,6 @@
+using Serilog;
+using Serilog.Events;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,29 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Load InfluxDB configuration
+var influxDbConfig = builder.Configuration.GetSection("InfluxDB");
+var influxUrl = influxDbConfig["Url"];
+var influxToken = influxDbConfig["Token"];
+var influxOrg = influxDbConfig["Org"];
+var influxBucket = influxDbConfig["Bucket"];
+
+// Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.InfluxDB(
+        applicationName: "SimpleWebApi",
+        uri: new Uri(influxUrl),
+        organizationId: influxOrg,
+        bucketName: influxBucket,
+        token: influxToken
+    )
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 WebApplication app = builder.Build();
 
